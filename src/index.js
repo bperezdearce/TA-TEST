4179bf0a5032a5dd13f60cfa8ce3404053f47bd8
Bárbara Pérez de Arce <barbara.perezdearce@gmail.com>
2024-08-09 20:11:41 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ const server = express();
 server.use(express.json);
 server.use(indexRouter)
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5147;
 
 server.listen(port);
